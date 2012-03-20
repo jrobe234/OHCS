88e26d7458b8add0ff15c9fdb26cd29e294c7416
James Robertson <jrobe234@gmail.com>
2012-03-20 12:37:06 +1000

    changes

---
+++ web/WEB-INF/results/markers.jsp
@@ -0,0 +1,36 @@
+<%@page import="java.sql.*"%>
+<%@page import="utilities.*"%>
+<%@page contentType="text/html; charset=UTF-8"%>
+<%@page import="org.json.simple.*"%>
+<%
+    String north = request.getParameter("north").toString();
+    String south = request.getParameter("south").toString();
+    String east = request.getParameter("east").toString();
+    String west = request.getParameter("west").toString();
+    if (north != null && south != null && east != null && west != null) {
+        JSONObject obj = new JSONObject();
+        JSONArray markers = new JSONArray();
+        JSONObject marker;
+
+        dbConnect conn = new dbConnect();
+        
+        String query = "SELECT * FROM HEALTHCARESERVICES INNER JOIN LOCATION ON HEALTHCARESERVICES.LOCATIONID = LOCATION.LOCATIONID WHERE ";
+        
+        query += "latitude < " + north + " AND latitude > " + south + " AND longitude < " + east + " AND longitude > " + west + "";
+        
+        ResultSet results = conn.executeQuery(query);
+        while (results.next()) {
+            marker = new JSONObject();
+            marker.put("ID", results.getString("healthcareID"));
+            marker.put("name", results.getString("name"));
+           marker.put("phone", results.getString("phone"));
+            marker.put("longitude", results.getString("LONGITUDE"));
+            marker.put("latitude", results.getString("LATITUDE"));
+            markers.add(marker);
+        }
+        obj.put("markers", markers);
+        response.setContentType("application/json");
+        response.getWriter().write(obj.toString());
+    }
+
+%>
---
+++ web/includes/js/map.js
@@ -0,0 +1,80 @@
+function getMax(center, bounds){
+    var ne = new google.maps.LatLng(bounds.getNorthEast().lat(), bounds.getNorthEast().lng());
+    var sw = new google.maps.LatLng(bounds.getSouthWest().lat(), bounds.getSouthWest().lng());
+    var maxBounds = new google.maps.LatLngBounds(sw, ne);
+    return maxBounds;
+}
+
+function initialize() {
+    var defaultBounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(-10.4121, 113.0918),
+    new google.maps.LatLng(-43.3840, 153.023036)
+);
+        
+    var mapOptions = {
+        center: new google.maps.LatLng(-27.470280,153.023036),
+        zoom: 14,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    
+    var options = {
+        bounds: defaultBounds
+    }
+    /**
+   downloadUrl("markers", function(data) {
+      var markers = data.documentElement.getElementsByTagName("marker");
+      for (var i = 0; i < markers.length; i++) {
+        var latlng = new google.maps.LatLng(parseFloat(markers[i].getAttribute("latitude")),
+                                    parseFloat(markers[i].getAttribute("longitude")));
+        var marker = new google.maps.Marker({position: latlng, map: map});
+       }
+     });*/
+    
+       
+    var input = document.getElementById('searchTextField');
+    var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+    autocomplete.bindTo('bounds', map);
+
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        var place = autocomplete.getPlace();
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(14);
+        }
+    });
+    
+    google.maps.event.addListener(map, 'idle', function() {
+        var center = map.getCenter();
+        var bounds = map.getBounds();
+        var maxBounds = getMax(center, bounds);
+        //alert("north=" + maxBounds.getNorthEast().lat() + ";east=" + maxBounds.getNorthEast().lng() + ";south=" + maxBounds.getSouthWest().lat() + ";west=" + maxBounds.getSouthWest().lng() + "");
+        $.post(
+        "markers",
+        {
+            "north": maxBounds.getNorthEast().lat(),
+            "east": maxBounds.getNorthEast().lng(),
+            "south": maxBounds.getSouthWest().lat(),
+            "west": maxBounds.getSouthWest().lng()
+        },
+        function(data){
+            var markers = data.markers;
+            var list = "";
+            for (var i = 0; i < markers.length; i++) {
+                list = list +  "<div class=\"listItem\">" + markers[i].name + "</div>";
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(markers[i].latitude, markers[i].longitude),
+                    map: map
+                })
+            }
+            document.getElementById('list').innerHTML = list;
+        },
+        "json"
+        )
+    });
+    
+}
+google.maps.event.addDomListener(window, 'load', initialize);
